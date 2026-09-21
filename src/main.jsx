d8b5c1d810b74c7880a2a015727f11dc2065946c
Peter Gyorgy <35877421+gyorgyp@users.noreply.github.com>
2026-09-21 21:16:30 +0200

    Resolve merge conflict in main.jsx
    Removed conflicting code and retained the updated render statement.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,5 @@
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
-
 
 createRoot(document.getElementById('root')).render(
   <h1>Hello React!!</h1>
-)  
-=======
-
-createRoot(document.getElementById('root')).render(
-  <h1>Hello React!</h1>
 )
->>>>>>> 67774d764d3e4391fb56ac33a8715921bba6d1e3
