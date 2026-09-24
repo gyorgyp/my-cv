9b28c219ba1d87b95efc4c2646318ba20298f33c
Peter Gyorgy <gyorgyp@gmail.com>
2026-09-24 22:43:58 +0200

    config depends on github or azure cloud

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-cv"
+  base: isGithubPages ? "/my-cv" : "/my-cv"
 })
